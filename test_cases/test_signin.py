614697ce8b5ce7981c3d4140acd178cceccbf9ab
huyuqi <yuqi.hu@gmail.com>
2021-03-13 00:32:23 +0800

    update

--- test_cases/test_signin.py
+++ test_cases/test_signin.py
@@ -1,11 +1,14 @@
+import allure
 from test_sanxaod_po.main_page import MainPage
 
 
+@allure.feature('测试三小D登录浏览可打印模型列表')
 class TestSignIn():
 
     def setup(self):
         self.main = MainPage()
 
+    @allure.story('Test key word 登录查看')
     def test_login(self):
         result = self.main.goto_print_list_ava().page_scroll()
 
